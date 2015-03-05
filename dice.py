98411e3441b2e6c8656f24642e99d07149d7f838
mobluse <mobluse@users.noreply.github.com>
2015-03-05 02:32:24 +0100

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-# Dice (sv. Tärning)
-# License: GPLv3+ ABSOLUTELY NO WARRANTY
-# Author: Mikael O. Bonnier, Lund, Sweden. mobluse on Scratch & GitHub
-
-import RPi.GPIO as GPIO 
+# Dice (sv. Tärning) Copyright (C) 2015 Mikael O. Bonnier, Lund, Sweden. mobluse on Scratch & GitHub
+# This program comes with ABSOLUTELY NO WARRANTY.
+# This is free software, and you are welcome to redistribute it under certain conditions.
+# License: GPLv3+ http://www.gnu.org/copyleft/gpl.html
 # Runs on Raspberry Pi 1 A & B Rev. 2.
 # (sv. Fungerar på Raspberry Pi 1 A & B Rev. 2.)
+# (sv. Utrustning: 5 sladdar hona-hona, 1 LED, 1 resistor 330 ohm, 1 del av motståndsben.)
+
+import RPi.GPIO as GPIO 
 import time
 import random
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.IN) # Pin 3 (sv. Pinne 3)
 # Pin 6 is GND. (sv. Pinne 6 är jord.)
+# (sv. koppla sladdar till pinnarna och en del av ett komponentben till en sladd som prob.
+#  Peta med komponentbenet på metalldelen på den andra sladden för att kasta tärningen.) 
 GPIO.setup(23, GPIO.OUT) # Pin 16 (sv. Pinne 16)
 # Pin 14 is GND. (sv. Pinne 14 är jord.)
+# (sv. Koppla in lysdiod och resistor i serie mellan pinnarna med 3 sladdar.)
 
 for i in range(3): # Danish salute! (sv. Dansk lösen!)
     GPIO.output(23, True)
